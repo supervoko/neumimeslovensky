16de74aa78f4e67acc4e5a6ab344fc0a4bafb66a
supervoko <64594576+supervoko@users.noreply.github.com>
2022-08-10 15:00:05 +0200

    base for abbreviation translation
    creates working base for abbreviation translation, removed some coments

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 import re
 
-
 window = tk.Tk()
 window.title("neumimslovensky")
 window.geometry("400x400")
@@ -45,7 +44,6 @@ def close_window():
 button3 = tk.Button(window, text="exit", command=close_window)
 button3.pack()
 
-
 # add text input with up to 10000 characters
 text_input = tk.Text(window, width=100, height=15)
 text_input.pack()
@@ -66,28 +64,14 @@ def results():
     text_input_value = text_input.get("1.0", "end-1c")
     # create string with regex (?<==')(.*?)(?='>) to extract text between brackets
     text_results_value = re.findall(r'''(?<==\')(.*?)(?=\')''', str(text_input_value))
-    # this is just temporarily to test if the regex works
-    # paste text_results_value into text_results
-    text_results.insert(tk.END, text_results_value)
-
-    # ____________________________________________________________________________________________________________
-    # CAREFUL! I SPEND 3 HOURS ON THIS PART AND I WASN'T ABLE TO FIND A WAY TO GET IT TO WORK
-    # ____________________________________________________________________________________________________________
-    # I tried to use loop with two list, but it didn't work
-
-    # create two lists: list1 and list2
-    # define edited_text_results_value where everything in text_results_value from list1
-    # is replaced with items from list2, and then put it in text_results
-    # text_results_value_str = str(text_results_value)
-    # a = ["po", "pt"]
-    # b = ["podmět", "předmět"]
-    # for i in range(len(a)):
-    #     if b[i] in text_results_value_str:
-    #         text_results_value_str = text_results_value_str.replace(b[i], a[i])
-    # print(text_results_value_str)
-    # ____________________________________________________________________________________________________________
-    # END OF CAREFUL PART
-    # ____________________________________________________________________________________________________________
+    # translates abbreviations
+    text_results_value_str = str(text_results_value)
+    a = ["věta hlavní", "věta vedlejší", "podmět"]
+    b = ["vh", "vv", "po"]
+    for x in range(len(a)):
+        text_results_value_str = text_results_value_str.replace(b[x], a[x])
+    # packs translated text to output
+    text_results.insert(tk.END, text_results_value_str)
 
     # create button to close results window
     button_results = tk.Button(window_results, text="close", command=window_results.destroy)
@@ -112,15 +96,14 @@ def submit_text():
         green_submit_button()
         # print("correct")
         results()
+    elif text_input_value.startswith("{") and text_input_value.endswith("}") and text_input_value[5] == "i":
+        green_submit_button()
+        # print("correct")
+        results()
     else:
-        if text_input_value.startswith("{") and text_input_value.endswith("}") and text_input_value[5] == "i":
-            green_submit_button()
-            # print("correct")
-            results()
-        else:
-            red_submit_button()
-            # print("incorrect")
-            # exit()
+        red_submit_button()
+        # print("incorrect")
+        # exit()
 
 
 # SUBMIT BUTTON
@@ -130,3 +113,4 @@ button_submit.pack()
 
 # start window
 window.mainloop()
+# print hello world
