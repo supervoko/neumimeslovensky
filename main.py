3e5f4df20a15681d788aee85d0b79211f1265dbc
supervoko <vojta.voko@gmail.com>
2023-07-05 17:41:24 +0200

    překládá všechny komentáře do češtiny dělá komentáře mnohem užitečnější pro pochopení kódu překládá funkci stupid2 na rundisc, pro lepší pochopení kódu

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
-import tkinter as tk
-import re
+# importuje nutné moduly
+import tkinter as tk        # pro práci s okny
+import re                   # pro získání důležitých dat z inputu
 
 
 def main(is_debug_mode):
     def submit_text_a(is_debug_mode):
+        # každá hodnota každého indexu listu A koresponduje s hodnotou stejného indexu listu B
+        # jedná se jednoduchou databázi, pomocí které program
+        # přeložené názvy očekávaných názvů vstupu
         a = ["věta hlavní", "věta vedlejší", "podmět", "předmět", "přívlastek shodný",
              "přívlastek neshodný", "přísudek", "příslovečné určení místa", "příslovečné určení způsobu", "doplněk"]
-
+        # očekávané zkratkové názvy vstupu
         b = ["vh", "vv", "po", "pt", "pks", "pkn", "ps", "pum", "puz", "dop"]
         submit_text(a, b, is_debug_mode)
 
     window = tk.Tk()
-    window.title("neumimslovensky")
+    window.title("neumimeslovensky")
     window.geometry("400x400")
     window.resizable(False, False)
 
-    # main text input, we will be also pasting into that
+    # okno s textovým políčkem pro input
     text_input = tk.Text(window, width=100, height=15)
     text_input.pack()
 
-    # clear text input function
+    # funkce pro odstranění všeho textu
     def clear_text():
         text_input.delete("1.0", "end")
 
-    # if button clear_text is pressed, clear text input
+    # tlačítko se zavoláním funkce pro odstranění textu
     button_clear_text = tk.Button(window, text="clear text", command=clear_text)
     button_clear_text.pack()
 
-    # pastes into text input the clipboard data
+    # funkce a tlačítko pro zavolání funkce pro
+    # vložení schránky do inputu
+    # POTENCIÁLNÍ PORUŠENÍ BEZPEČNOSTI A NARUŠENÍ SOUKROMÍ
     def paste_clipboard():
         clipboard_text = window.clipboard_get()
         text_input.insert(tk.END, clipboard_text)
@@ -35,97 +41,107 @@ def main(is_debug_mode):
     button_paste = tk.Button(window, text="paste from clipboard", command=paste_clipboard)
     button_paste.pack()
 
-    # crates new window that displays a little edited text input value while
+    # vznik okna pro zobrazení výstupu
     def results(a, b, is_debug_mode):
         window_results = tk.Tk()
         window_results.title("results")
         window_results.geometry("500x400")
-        # make results window resizable
         window_results.resizable(True, True)
-        # creates scrollable results window
-        scrollbar = tk.Scrollbar(window_results)
-        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        scrollbar = tk.Scrollbar(window_results) # přidá scrollbar
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y) # přidá scrollbar
         text_results = tk.Text(window_results, width=130, height=400)
         text_results.pack()
-        text_input_value = text_input.get("1.0", "end-1c")
-        # create string with regex (?<==')(.*?)(?='>) to extract text between brackets
+        #__________________________________________________________________________________________
+        text_input_value = text_input.get("1.0", "end-1c") # vytvoření krátkodobé proměné pro práci s ní
 
-        if is_debug_mode == 3:
+        # za pomoci regexu (?<==')(.*?)(?='>) extrahuje pouze důležitý text
+
+
+        if is_debug_mode == 3: # v případě zapnutého debug módu proces přeskočí
             text_results_value = str(text_input_value)
         else:
+            # za pomoci regexu nalezne všechna důležitá data a uschová je do krátkodobé proměn
             text_results_value = re.findall(r'''(?<==\')(.*?)(?=\')''', str(text_input_value))
 
-        # translates abbreviations
+        # překládá zkratková slova za pomoci listu A a listu B
 
-        text_results_value_str = str(text_results_value)
+        # NÁSLEDUJÍCÍ ŘÁDEK NEGATIVNĚ OVLIVŇUJE OUTPUT, NEBOŤ PO SOBĚ ZANECHÁVÁ PO SOBĚ PRVKY LISTU VE FORMĚ STRINGU
+        text_results_value_str = str(text_results_value) # změní list dat na string pro lepší manipulaci
+        # přeskočí krok, pokud je zapnutý debug mód
         if is_debug_mode == 4:
-            j = 136137138
+            pass
         else:
+            # loop, který projede keždý index v listu b
+            # a nahradí hodnotu toho indexu za hodnotu stejného indexu v listu a
             for x in range(len(a)):
                 text_results_value_str = text_results_value_str.replace(b[x], a[x])
-        # packs translated text to output
+        # strčí výsledek do dříve vytvořeného okna
         text_results.insert(tk.END, text_results_value_str)
-        # create button to close results window
+        # tlačítko pro zavření okna
         button_results = tk.Button(window_results, text="close", command=window_results.destroy)
         button_results.pack()
 
+    # funkce pro změnu barvy tlačítka v případě správného inputu
     def green_submit_button():
         button_submit.config(fg="green")
         button_submit.after(500, lambda: button_submit.config(fg="black"))
 
+    # funkce pro změnu barvy tlačítka v případě pravděpodobně špatného inputu
     def red_submit_button():
         button_submit.config(fg="red")
         button_submit.after(500, lambda: button_submit.config(fg="black"))
 
-    # SUBMIT BUTTON AND TEXT INPUT CONTROL
-    # if submit button is pressed, turn submit whole button color blue for 5 seconds and call submit_text_value function
+    # logika schvalování inputu
+    # pokud je stisknuto tlačítko submit,
+    # je zkontrolováno, zda je input správný a v případě že je,
+    # a zavolá se funkce results.
     def submit_text(a, b, is_debug_mode):
+        # v případě zaplého debug módu je celá logika přeskočena a funkce results je zavolána okamžitě
         if is_debug_mode < 2:
             text_input_value = text_input.get("1.0", "end-1c")
+
             if text_input_value.startswith("{") and text_input_value.endswith("}") and text_input_value[7] == "i":
                 green_submit_button()
-                # print("correct")
                 results(a, b, is_debug_mode)
+
             elif text_input_value.startswith("{") and text_input_value.endswith("}") and text_input_value[5] == "i":
                 green_submit_button()
-                # print("correct")
                 results(a, b, is_debug_mode)
+
             else:
                 red_submit_button()
-                # print("incorrect")
-                # exit()
+
         else:
             results(a, b, is_debug_mode)
 
-    # SUBMIT BUTTON
-    # add button under text input to submit and store text input in variable
+    # tlačítko submit
+    # vloží input do proměné a zavolá logiku pro schvalování inputu
     button_submit = tk.Button(window, text="submit", command=lambda: submit_text_a(is_debug_mode))
     button_submit.pack()
 
-    # if button close_window is pressed, close window
+    # tlačítko pro ukončení aplikace
     def close_window():
         window.destroy()
 
     button_exit = tk.Button(window, text="exit", command=close_window)
     button_exit.pack()
 
-    # start window
+    # zapne hlavní inputové
     window.mainloop()
-    # print hello world
 
 
-# create disclaimer popup window with text
+# vytvoří okno pro disclaimer a ukončí okno debug módu
 def disclaimer(is_debug_mode):
-    def stupid2(is_debug_mode):
+    def rundisc(is_debug_mode):
         window_disclaimer.destroy()
         main(is_debug_mode)
 
     window_disclaimer = tk.Tk()
     window_disclaimer.title("disclaimer")
     window_disclaimer.geometry("800x200")
-    # make disclaimer window not resizable
     window_disclaimer.resizable(False, False)
-    # disclaimer text
+    # text disclaimeru
+    # bylo by vhodné mít tento text uložený v samostatném soboru, nikoliv součástí aplikace
     disclaimer_text = tk.Label(window_disclaimer,
                                text="This application was created only for the purpose of "
                                     "improving the programming skills of the creators and "
@@ -142,11 +158,12 @@ def disclaimer(is_debug_mode):
                                     " take all responsibility for your "
                                     "use of this application and that you understand these terms.")
     disclaimer_text.pack()
-    button_disclaimer = tk.Button(window_disclaimer, text="I agree", command=lambda: stupid2(is_debug_mode))
+    button_disclaimer = tk.Button(window_disclaimer, text="I agree", command=lambda: rundisc(is_debug_mode))
     button_disclaimer.pack()
     window_disclaimer.mainloop()
 
-
+# okno pro zvolení debug módu
+# cílem výběru debug módu je, aby si uživatel mohl zvolit, pokud mu určitá část kódu způsobuje problémy
 def debug_mode():
     debug_mode_window = tk.Tk()
     debug_mode_window.title("debug mode")
@@ -181,13 +198,13 @@ def debug_mode():
         debug_mode_window.destroy()
         disclaimer(is_debug_mode)
 
-    # works normally
+    # funguje normálně
     debug_mode_button_no = tk.Button(debug_mode_window, text="no", command=func_no, font=('Arial', 20))
-    # ignores check for right input
+    # ignoruje kontrolu správného inputu
     debug_mode_button_1 = tk.Button(debug_mode_window, text="1", command=func_1)
-    # ignores check for right input and regex search
+    # ignoruje kontrolu správného inputu a zároveň přeskakuje filtraci regexem
     debug_mode_button_2 = tk.Button(debug_mode_window, text="2", command=func_2)
-    # ignores check for right input and abbreviation translation
+    # ignoruje kontrolu správného inputu a zároveň přeskakuje překládání zkratek
     debug_mode_button_3 = tk.Button(debug_mode_window, text="3", command=func_3)
     debug_mode_button_no.pack()
     debug_mode_button_1.pack()
@@ -195,5 +212,5 @@ def debug_mode():
     debug_mode_button_3.pack()
     debug_mode_window.mainloop()
 
-
+# zapne výběr debug módu a tím pádem celého programu
 debug_mode()
