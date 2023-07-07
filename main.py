ddcce432514fcf2a10e7fef9c2d86c7a5216b2be
supervoko <vojta.voko@gmail.com>
2023-07-07 18:28:02 +0200

    vyčištění kódu od většiny zbytečností, opraven bug nefunkční debug módu 1, zjednodušena logika vybírání debug módu, přidání lepšího disclaimeru

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 # importuje nutné moduly
-import tkinter as tk        # pro práci s okny
-import re                   # pro získání důležitých dat z inputu
+import tkinter as tk  # pro práci s okny
+import re  # pro získání důležitých dat z inputu
+
+# každá hodnota každého indexu listu A koresponduje s hodnotou stejného indexu listu B
+# jedná se jednoduchou databázi, pomocí které program
+
+# přeložené názvy očekávaných názvů vstupu
+a = ["věta hlavní", "věta vedlejší", "podmět", "předmět", "přívlastek shodný",
+     "přívlastek neshodný", "přísudek", "příslovečné určení místa", "příslovečné určení způsobu", "doplněk"]
+# očekávané zkratkové názvy vstupu
+b = ["vh", "vv", "po", "pt", "pks", "pkn", "ps", "pum", "puz", "dop"]
 
 
-def main(is_debug_mode):
-    def submit_text_a(is_debug_mode):
-        # každá hodnota každého indexu listu A koresponduje s hodnotou stejného indexu listu B
-        # jedná se jednoduchou databázi, pomocí které program
-        # přeložené názvy očekávaných názvů vstupu
-        a = ["věta hlavní", "věta vedlejší", "podmět", "předmět", "přívlastek shodný",
-             "přívlastek neshodný", "přísudek", "příslovečné určení místa", "příslovečné určení způsobu", "doplněk"]
-        # očekávané zkratkové názvy vstupu
-        b = ["vh", "vv", "po", "pt", "pks", "pkn", "ps", "pum", "puz", "dop"]
-        submit_text(a, b, is_debug_mode)
-
+def main():
     window = tk.Tk()
     window.title("neumimeslovensky")
     window.geometry("400x400")
@@ -42,22 +50,21 @@ def main(is_debug_mode):
     button_paste.pack()
 
     # vznik okna pro zobrazení výstupu
-    def results(a, b, is_debug_mode):
+    def results():
         window_results = tk.Tk()
         window_results.title("results")
         window_results.geometry("500x400")
         window_results.resizable(True, True)
-        scrollbar = tk.Scrollbar(window_results) # přidá scrollbar
-        scrollbar.pack(side=tk.RIGHT, fill=tk.Y) # přidá scrollbar
+        scrollbar = tk.Scrollbar(window_results)  # přidá scrollbar
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)  # přidá scrollbar
         text_results = tk.Text(window_results, width=130, height=400)
         text_results.pack()
-        #__________________________________________________________________________________________
-        text_input_value = text_input.get("1.0", "end-1c") # vytvoření krátkodobé proměné pro práci s ní
+        # __________________________________________________________________________________________
+        text_input_value = text_input.get("1.0", "end-1c")  # vytvoření krátkodobé proměné pro práci s ní
 
         # za pomoci regexu (?<==')(.*?)(?='>) extrahuje pouze důležitý text
 
-
-        if is_debug_mode == 3: # v případě zapnutého debug módu proces přeskočí
+        if is_debug_mode == 2:  # v případě zapnutého debug módu proces přeskočí
             text_results_value = str(text_input_value)
         else:
             # za pomoci regexu nalezne všechna důležitá data a uschová je do krátkodobé proměn
@@ -66,12 +73,12 @@ def main(is_debug_mode):
         # překládá zkratková slova za pomoci listu A a listu B
 
         # NÁSLEDUJÍCÍ ŘÁDEK NEGATIVNĚ OVLIVŇUJE OUTPUT, NEBOŤ PO SOBĚ ZANECHÁVÁ PO SOBĚ PRVKY LISTU VE FORMĚ STRINGU
-        text_results_value_str = str(text_results_value) # změní list dat na string pro lepší manipulaci
+        text_results_value_str = str(text_results_value)  # změní list dat na string pro lepší manipulaci
         # přeskočí krok, pokud je zapnutý debug mód
-        if is_debug_mode == 4:
+        if is_debug_mode == 3:
             pass
         else:
-            # loop, který projede keždý index v listu b
+            # loop, který projede každý index v listu b
             # a nahradí hodnotu toho indexu za hodnotu stejného indexu v listu a
             for x in range(len(a)):
                 text_results_value_str = text_results_value_str.replace(b[x], a[x])
@@ -95,28 +102,28 @@ def main(is_debug_mode):
     # pokud je stisknuto tlačítko submit,
     # je zkontrolováno, zda je input správný a v případě že je,
     # a zavolá se funkce results.
-    def submit_text(a, b, is_debug_mode):
+    def submit_text():
+        text_input_value = text_input.get("1.0", "end-1c")
         # v případě zaplého debug módu je celá logika přeskočena a funkce results je zavolána okamžitě
-        if is_debug_mode < 2:
-            text_input_value = text_input.get("1.0", "end-1c")
+        if is_debug_mode << 1:
 
             if text_input_value.startswith("{") and text_input_value.endswith("}") and text_input_value[7] == "i":
                 green_submit_button()
-                results(a, b, is_debug_mode)
+                results()
 
             elif text_input_value.startswith("{") and text_input_value.endswith("}") and text_input_value[5] == "i":
                 green_submit_button()
-                results(a, b, is_debug_mode)
+                results()
 
             else:
                 red_submit_button()
 
         else:
-            results(a, b, is_debug_mode)
+            results()
 
     # tlačítko submit
     # vloží input do proměné a zavolá logiku pro schvalování inputu
-    button_submit = tk.Button(window, text="submit", command=lambda: submit_text_a(is_debug_mode))
+    button_submit = tk.Button(window, text="submit", command=lambda: submit_text())
     button_submit.pack()
 
     # tlačítko pro ukončení aplikace
@@ -131,10 +138,10 @@ def main(is_debug_mode):
 
 
 # vytvoří okno pro disclaimer a ukončí okno debug módu
-def disclaimer(is_debug_mode):
-    def rundisc(is_debug_mode):
+def disclaimer():
+    def rundisc():
         window_disclaimer.destroy()
-        main(is_debug_mode)
+        main()
 
     window_disclaimer = tk.Tk()
     window_disclaimer.title("disclaimer")
@@ -143,24 +150,18 @@ def disclaimer(is_debug_mode):
     # text disclaimeru
     # bylo by vhodné mít tento text uložený v samostatném soboru, nikoliv součástí aplikace
     disclaimer_text = tk.Label(window_disclaimer,
-                               text="This application was created only for the purpose of "
-                                    "improving the programming skills of the creators and "
-                                    "at the same time as a proof of concept.\n"
-                                    " It is not intended for any use with other applications,"
-                                    " \nservices, websites "
-                                    "and files or text obtained from them, and such use is "
-                                    "also prohibited by the creator. \n"
-                                    "The creators waive any responsibility for the misuse of the "
-                                    "program by the user. \n"
-                                    "By clicking the \"I agree\" button, you confirm that you"
-                                    " will not misuse the program "
-                                    "for any reason other than testing reasons, \nthat you"
-                                    " take all responsibility for your "
-                                    "use of this application and that you understand these terms.")
+                               text="""THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.""")
     disclaimer_text.pack()
-    button_disclaimer = tk.Button(window_disclaimer, text="I agree", command=lambda: rundisc(is_debug_mode))
+    button_disclaimer = tk.Button(window_disclaimer, text="I agree", command=lambda: rundisc())
     button_disclaimer.pack()
     window_disclaimer.mainloop()
+
 
 # okno pro zvolení debug módu
 # cílem výběru debug módu je, aby si uživatel mohl zvolit, pokud mu určitá část kódu způsobuje problémy
@@ -177,26 +178,26 @@ def debug_mode():
                                 font=('Arial', 8))
     debug_mode_text.pack()
     debug_mode_text2.pack()
-
+    global is_debug_mode
+    is_debug_mode = 0
     def func_no():
-        is_debug_mode = 1
         debug_mode_window.destroy()
-        disclaimer(is_debug_mode)
+        disclaimer()
 
     def func_1():
-        is_debug_mode = 2
+        is_debug_mode = 1
         debug_mode_window.destroy()
-        disclaimer(is_debug_mode)
+        disclaimer()
 
     def func_2():
-        is_debug_mode = 3
+        is_debug_mode = 2
         debug_mode_window.destroy()
-        disclaimer(is_debug_mode)
+        disclaimer()
 
     def func_3():
-        is_debug_mode = 4
+        is_debug_mode = 3
         debug_mode_window.destroy()
-        disclaimer(is_debug_mode)
+        disclaimer()
 
     # funguje normálně
     debug_mode_button_no = tk.Button(debug_mode_window, text="no", command=func_no, font=('Arial', 20))
@@ -211,6 +212,7 @@ def debug_mode():
     debug_mode_button_2.pack()
     debug_mode_button_3.pack()
     debug_mode_window.mainloop()
+
 
 # zapne výběr debug módu a tím pádem celého programu
 debug_mode()
